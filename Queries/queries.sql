111a6dfb76ce20a7ffb1c6864b36ea70781f2b11
c2j185 <staceylconley@outlook.com>
2021-08-15 19:12:10 -0400

    a

---
+++ Queries/queries.sql
@@ -0,0 +1,57 @@
+create table dept_employee(
+	emp_no int not null,
+	dept_no varchar not null,
+	from_date date not null,
+	primary key (dept_no)
+	);
+
+create table titles(
+	emp_no int not null,
+	title varchar not null,
+	from_date date not null,
+	to_date date not null,
+	primary key (title)
+	);
+
+select * from dept_manager
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31';
+
+-- Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+--Create new table from info
+SELECT first_name, last_name
+into retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+select * from retirement_info
